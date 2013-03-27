dc2343240da88e41548a67eefe28de12d0d3b79e
time <T.Evans@ic.ac.uk>
2013-03-27 18:06:26 +0000

    Added Elsevier routines to simplify networks.

---
+++ JAVA/Elsevier/src/ebrp/ElsevierStopStems.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ebrp;
+
+/**
+ *
+ * @author time
+ */
+public class ElsevierStopStems {
+    /**
+     * An array containing some common Physics stems.
+     * <p>Taken from most popular stems listed in
+     * <tt>ebrp_03_set_01_documents_2010_Phy_PTt_T1_TERMvertices.xlsx</tt>
+     */
+    public static final String[] PHYSICS={"effect",
+                                    "properti",
+                                    "studi",
+                                    "model",
+                                    "system",
+                                    "structur",
+                                    "base",
+                                    "optic",
+                                    "analysi",
+                                    "method",
+                                    "film",
+                                    "laser",
+                                    "measur",
+                                    "applic",
+                                    "surfac",
+                                    "magnet",
+                                    "imag",
+                                    "crystal",
+                                    "high",
+                                    "alloi",
+                                    "simul",
+                                    "character",
+                                    "synthesi",
+                                    "temperatur",
+                                    "process",
+                                    "mechan",
+                                    "field",
+                                    "phase",
+                                    "composit",
+                                    "thermal",
+                                    "dynam",
+                                    "design",
+                                    "electron",
+                                    "carbon",
+                                    "thin",
+                                    "flow",
+                                    "prepar"} ; 
+    
+    /**
+     * An array containing some common Business stems.
+     * <p>Taken from most popular stems listed in
+     * <tt>ebrp_03_set_01_documents_2010_Phy_PTt_T1_TERMvertices.xlsx</tt>
+     */
+    public static final String[] BUSINESS={};
+    
+}
